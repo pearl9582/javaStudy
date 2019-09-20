ea4e84967a79d85d28abee692cb9523555dd263d
DESKTOP-8RA7SKQ\pearl <geng_ping@yeah.net>
2019-09-20 22:07:13 +0800

    插入排序

--- src/com/gp/sort/Sort.java
+++ src/com/gp/sort/Sort.java
@@ -1,4 +1,40 @@
 package com.gp.sort;
 
 public class Sort {
+    static void swap(int[] nums, int a, int b){
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+    static void printNums(int[] nums){
+        for(int i = 0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
+    }
+    //插入排序
+    static void insertSort(int[] nums) {
+        int len = nums.length;
+        for (int i = 1; i < len; i++) {
+            int cur = nums[i];
+            int j = i-1;
+            while(j>=0 && nums[j] > cur){
+                nums[j+1] = nums[j];
+                j--;
+            }
+            nums[j+1] = cur;
+        }
+    }
+
+    //希尔排序
+    static void shellSort(int[] nums){
+
+    }
+
+
+    public static void main(String[] args){
+        int[] nums = new int[]{3, 1, 7, 4, 9, 5, 0, 8, 2, 6};
+        insertSort(nums);
+        printNums(nums);
+    }
 }
