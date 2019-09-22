d34c4fcf1182856ad6f726fe7873e12131550778
pearl <geng_ping@yeah.net>
2019-09-22 10:21:01 +0800

    网格从左上角到右下角有多少种路线

---
+++ src/com/gp/dp/GridWalk.java
@@ -0,0 +1,41 @@
+package com.gp.dp;
+
+import java.util.Scanner;
+
+/**
+ * @Author Pearl
+ * @Description:  从n*m的网格的左上角走到右下角，有多少种方法
+ * @Date 2019/9/22上午 10:09
+ */
+public class GridWalk {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        System.out.println(count(n,m));
+    }
+
+    //问题一
+    private static int count(int x, int y){
+        if(x == 1 || y == 1) {
+            return 1;
+        } else{
+            return count(x-1,y)+count(x,y-1);
+        }
+    }
+    static int count2(int n, int m ){
+        int[][] dp = new int[n][m];
+        for(int i = 0;i<n;i++){
+            dp[0][i] = 1;
+        }
+        for(int i = 0;i<m;i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1 ; i<n;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
+    }
+}
