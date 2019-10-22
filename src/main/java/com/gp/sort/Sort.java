ade9aebbd7aa7b795c997d53cabd9d2e0eebf6e6
DESKTOP-8RA7SKQ\pearl <geng_ping@yeah.net>
2019-10-22 20:22:20 +0800

    update

--- src/main/java/com/gp/sort/Sort.java
+++ src/main/java/com/gp/sort/Sort.java
@@ -31,10 +31,87 @@ public class Sort {
 
     }
 
+    /**
+     * 冒泡排序
+     *
+     * @param array
+     * @return
+     */
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j + 1] < array[j]) {
+                    swap(array, j,j+1);
+                }
+            }
+        }
+    }
+
+    /**
+     * 选择排序
+     * @param array
+     * @return
+     */
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) { //找到最小的数
+                    minIndex = j; //将最小数的索引保存
+                }
+                swap(array, minIndex, i);
+            }
+        }
+    }
+
+
+
+    // 查找出中轴（默认是最低位low）的在numbers数组排序后所在位置
+    public static int getMiddle(int[] numbers, int low,int high){
+        //数组的第一个作为中轴
+        int temp = numbers[low];
+        while(low < high){
+            while(low < high && numbers[high] > temp){
+                high--;
+            }
+            //比中轴小的记录移到低端
+            numbers[low] = numbers[high];
+            while(low < high && numbers[low] < temp){
+                low++;
+            }
+            //比中轴大的记录移到高端
+            numbers[high] = numbers[low] ;
+        }
+        //中轴记录到尾
+        numbers[low] = temp ;
+        // 返回中轴的位置
+        return low ;
+    }
+    public static void quickSort(int[] numbers,int low,int high)
+    {
+        if(low < high)
+        {
+            //将numbers数组进行一分为二
+            int middle = getMiddle(numbers,low,high);
+            //对低字段表进行递归排序
+            quickSort(numbers, low, middle-1);
+            //对高字段表进行递归排序
+            quickSort(numbers, middle+1, high);
+        }
+    }
+
+    public static void quick(int[] numbers)
+    {
+        //查看数组是否为空
+        if(numbers.length > 0)
+        {
+            quickSort(numbers, 0, numbers.length-1);
+        }
+    }
 
     public static void main(String[] args){
         int[] nums = new int[]{3, 1, 7, 4, 9, 5, 0, 8, 2, 6};
-        insertSort(nums);
+        bubbleSort(nums);
         printNums(nums);
     }
 }
